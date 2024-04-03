274c12522c37a1fe0e46da2fa35029df131f11c9
SatakeYusuke19920527 <satkyusk@icloud.com>
2024-04-04 08:42:32 +0900

    first commit

---
+++ environment/variables.tf
@@ -0,0 +1,16 @@
+variable "rg_name" {
+  type = string
+}
+
+variable "rg_location" {
+  type = string
+}
+
+variable "swa_name" {
+  type = string
+  default = "eastasia"
+}
+
+variable "swa_location" {
+  type = string
+}
